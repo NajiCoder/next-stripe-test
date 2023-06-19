f5ab55a1b2f2215c9f90bf3625e18e5a379c8747
Naji <lemiine987@gmail.com>
2023-06-19 17:04:34 +0300

    Integrated stripe payment

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { checkout } from "../checkout";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,7 +10,7 @@ export default function Home() {
         <h1 className="text-7xl font-bold">Buy my stupid shit</h1>
       </div>
       <div className="flex gap-6">
-        <div className="flex flex-col gap-2 group border-2 hover:border-blue-300 p-10 ">
+        <div className="flex flex-col items-center gap-2 group border-2 hover:border-blue-300 p-10 ">
           <Image
             src="/images/image-1.avif"
             alt="image1"
@@ -17,8 +20,23 @@ export default function Home() {
           <p className="group-hover:text-rose-300">
             expencive stolen picture 1
           </p>
+          <button
+            className="w-1/3 bg-rose-300 rounded-md hover:bg-rose-100"
+            onClick={() =>
+              checkout({
+                lineItems: [
+                  {
+                    price: process.env.NEXT_PUBLIC_IMAGE1_PRICE,
+                    quantity: 1,
+                  },
+                ],
+              })
+            }
+          >
+            Buy!
+          </button>
         </div>
-        <div className="flex flex-col gap-2 group border-2 hover:border-blue-300 p-10">
+        <div className="flex flex-col items-center gap-2 group border-2 hover:border-blue-300 p-10">
           <Image
             src="/images/image-2.avif"
             alt="image1"
@@ -28,6 +46,21 @@ export default function Home() {
           <p className="group-hover:text-rose-300">
             expencive stolen picture 2
           </p>
+          <button
+            className="w-1/3 bg-rose-300 rounded-md hover:bg-rose-100"
+            onClick={() =>
+              checkout({
+                lineItems: [
+                  {
+                    price: process.env.NEXT_PUBLIC_IMAGE1_PRICE,
+                    quantity: 1,
+                  },
+                ],
+              })
+            }
+          >
+            Buy!
+          </button>
         </div>
       </div>
     </main>
